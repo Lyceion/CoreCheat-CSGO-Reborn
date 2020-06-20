7cc27d38dd01874b56424be8dc936f60db580547
Can <lyceion@protonmail.com>
2020-06-20 13:42:48 +0300

    client_panorama.dll -> client.dll (3)

--- Project_OpenSource/CoreCheat-CSGO-Reborn-master/Versions/OnlyWall/Launcher.cs
+++ Project_OpenSource/CoreCheat-CSGO-Reborn-master/Versions/OnlyWall/Launcher.cs
@@ -47,7 +47,7 @@ namespace CoreCheat_Reborn.Versions.OnlyWall
                 {
                     foreach (ProcessModule m in p[0].Modules)
                     {
-                        if (m.ModuleName == "client_panorama.dll")
+                        if (m.ModuleName == "client.dll")
                         {
                             Client = (int)m.BaseAddress;
                             return true;
