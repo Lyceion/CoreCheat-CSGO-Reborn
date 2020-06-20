6192377881d057506ccb9cba0ff7db8f19844642
Can <lyceion@protonmail.com>
2020-06-20 13:43:36 +0300

    Update Launcher.cs

--- Project_OpenSource/CoreCheat-CSGO-Reborn-master/Versions/Panorama/Launcher.cs
+++ Project_OpenSource/CoreCheat-CSGO-Reborn-master/Versions/Panorama/Launcher.cs
@@ -55,7 +55,7 @@ namespace CoreCheat_Reborn.Versions.Panorama
                 {
                     foreach (ProcessModule m in p[0].Modules)
                     {
-                        if (m.ModuleName == "client_panorama.dll")
+                        if (m.ModuleName == "client.dll")
                         {
                             Client = (int)m.BaseAddress;
                             return true;
